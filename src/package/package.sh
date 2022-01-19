f4d61d9209b985606bebd9786e22d6feadb09a09
Tahina Ramananandro <taramana@microsoft.com>
2022-01-19 13:37:57 -0800

    `make everparse` outside of a git working copy

--- src/package/package.sh
+++ src/package/package.sh
@@ -81,9 +81,10 @@ print_date_utc_of_iso_hr() {
 if [[ -z "$everparse_version" ]] ; then
     everparse_version=$(cat $QD_HOME/version.txt)
     everparse_last_version=$(git show --no-patch --format=%h $everparse_version || true)
-    everparse_commit=$(git show --no-patch --format=%h)
-    if [[ $everparse_commit != $everparse_last_version ]] ; then
-        everparse_version=$everparse_commit
+    if everparse_commit=$(git show --no-patch --format=%h) ; then
+        if [[ $everparse_commit != $everparse_last_version ]] ; then
+            everparse_version=$everparse_commit
+        fi
     fi
 fi
 
