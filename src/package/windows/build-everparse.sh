111ea442bd80fcdfd0b49912ee24e1d4771917da
Tahina Ramananandro <taramana@microsoft.com>
2022-01-19 13:46:37 -0800

    build-everparse.sh: what if z3 downloaded by `make everparse`?

--- src/package/windows/build-everparse.sh
+++ src/package/windows/build-everparse.sh
@@ -6,10 +6,11 @@ set -x
 unset CDPATH
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 pwd | grep '/src/package/windows$' > /dev/null
-z3_dir=$(dirname $(which z3.exe))
-cp $z3_dir/../lib/*.dll $z3_dir || true
-touch $z3_dir/z3dummy.lib
+if z3_dir=$(dirname $(which z3.exe)) ; then
+    cp $z3_dir/../lib/*.dll $z3_dir || true
+    touch $z3_dir/z3dummy.lib
+fi
 cd ../../..
 rm -rf everparse
 make everparse
-rm everparse/bin/z3dummy.lib
+rm -f everparse/bin/z3dummy.lib
