8e972d3e99e15b46c695bc4a9d21b468dec07841
Tahina Ramananandro <taramana@microsoft.com>
2022-12-02 17:02:17 -0800

    bypass git ownership check

--- src/package/windows/build-everparse.sh
+++ src/package/windows/build-everparse.sh
@@ -7,6 +7,8 @@ unset CDPATH
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 pwd | grep '/src/package/windows$' > /dev/null
 cd ../../..
+# This config is necessary if everparse was cloned with non-Cygwin git
+git config --global --add safe.directory $(pwd)
 git submodule update --init || true
 rm -rf everparse
 env EVERPARSE_MAKE_OPTS='-j 12' make everparse
