8cdba4bb783adf22a335017a7f5a4872ec0e0ebc
Tahina Ramananandro <taramana@microsoft.com>
2022-12-05 09:44:31 -0800

    release/build fixes on Windows

--- src/package/windows/build-everparse.ps1
+++ src/package/windows/build-everparse.ps1
@@ -1,6 +1,16 @@
 # This script installs EverParse build dependencies (including Cygwin)
 # and builds EverParse.
 
+# Choose a temporary directory name for Cygwin
+$tmpRoot = "C:\"
+[string] $tmpBaseName = "everparse-cygwin64.tmp"
+New-Item -Path $tmpRoot -Name $tmpBaseName -ItemType directory
+if (-not $?) {
+   $Error
+   exit 1
+}
+$Global:cygwinRoot = (Join-Path $tmpRoot $tmpBaseName)
+
 function global:Invoke-BashCmd
 {
     # This function invokes a Bash command via Cygwin bash.
@@ -9,8 +19,11 @@ function global:Invoke-BashCmd
     Write-Host "Args:" $args
 
     # Exec command
-    $cygpath = c:\cygwin64\bin\cygpath.exe -u ${pwd}
-    c:\cygwin64\bin\bash.exe --login -c "cd $cygpath && $args"
+    $cygwinRoot = $Global:cygwinRoot
+    $cygpathExe = "$cygwinRoot\bin\cygpath.exe"
+    $cygpath = & $cygpathExe -u ${pwd}
+    $bashExe = "$cygwinRoot\bin\bash.exe"
+    & $bashExe --login -c "cd $cygpath && $args"
 
     if (-not $?) {
         Write-Host "*** Error:"
@@ -33,7 +46,7 @@ Set-Location -ErrorAction Stop -LiteralPath $PSScriptRoot
 $Error.Clear()
 Write-Host "Install Cygwin with git"
 Invoke-WebRequest "https://www.cygwin.com/setup-x86_64.exe" -outfile "cygwinsetup.exe"
-cmd.exe /c start /wait .\cygwinsetup.exe --root C:\cygwin64 -P git,wget --no-desktop --no-shortcuts --no-startmenu --wait --quiet-mode --site https://mirrors.kernel.org/sourceware/cygwin/
+cmd.exe /c start /wait .\cygwinsetup.exe --root $Global:cygwinRoot -P git,wget --no-desktop --no-shortcuts --no-startmenu --wait --quiet-mode --site https://mirrors.kernel.org/sourceware/cygwin/
 if (-not $?) {
     $Error
     exit 1
@@ -51,6 +64,14 @@ if (-not $?) {
 $Error.Clear()
 Write-Host "build everparse"
 Invoke-BashCmd "./build-everparse.sh"
+if (-not $?) {
+    $Error
+    exit 1
+}
+
+$Error.Clear()
+Write-Host "remove our copy of Cygwin"
+Remove-Item -Recurse -Force -Path $Global:cygwinRoot
 if (-not $?) {
     $Error
     exit 1
--- src/package/windows/build-everparse.sh
+++ src/package/windows/build-everparse.sh
@@ -7,6 +7,12 @@ unset CDPATH
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 pwd | grep '/src/package/windows$' > /dev/null
 cd ../../..
-git submodule update --init || true
-rm -rf everparse
+# This config is necessary if everparse was cloned with non-Cygwin git
+git config --global --add safe.directory $(pwd)
+# Revert back to a clean working copy
+git clean -ffdx
+rm -rf FStar karamel hacl-star
+git reset HEAD -- .
+git checkout .
+git submodule update --init
 env EVERPARSE_MAKE_OPTS='-j 12' make everparse
