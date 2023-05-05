437cd34b9a88daf1f8ead6b094c8c73d22b8678c
Tahina Ramananandro <taramana@microsoft.com>
2023-05-04 18:58:14 -0700

    standalone CI: install Steel via install-deps.sh

--- .docker/build/build-standalone.sh
+++ .docker/build/build-standalone.sh
@@ -10,6 +10,12 @@ branchname=$2
 build_home="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "$build_home"/build_funs.sh
 
+function fetch_and_make_steel() {
+    # steel is already supposed to have been built and fetched before
+    # (e.g. by install-deps.sh)
+    true
+}
+
 rootPath=$(pwd)
 result_file="result.txt"
 status_file="status.txt"
