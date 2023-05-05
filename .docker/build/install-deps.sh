b3d6525af4afb496fa8c65530b88656a15fa9d69
Tahina Ramananandro <taramana@microsoft.com>
2023-05-04 18:58:14 -0700

    standalone CI: install Steel via install-deps.sh

--- .docker/build/install-deps.sh
+++ .docker/build/install-deps.sh
@@ -19,5 +19,15 @@ git clone --branch $FSTAR_BRANCH https://github.com/FStarLang/FStar "$FSTAR_HOME
 opam install --deps-only "$FSTAR_HOME/fstar.opam"
 OTHERFLAGS='--admit_smt_queries true' make -j 24 -C "$FSTAR_HOME"
 
+# Identify the Steel branch
+if [[ -z "$STEEL_BRANCH" ]] ; then
+STEEL_BRANCH=$(jq -c -r '.RepoVersions.steel_version' "$build_home"/config.json | sed 's!^origin/!!')
+fi
+
+# Install Steel and its dependencies
+[[ -n "$STEEL_HOME" ]]
+git clone --branch $STEEL_BRANCH https://github.com/FStarLang/steel "$STEEL_HOME"
+OTHERFLAGS='--admit_smt_queries true' make -j 24 -C "$STEEL_HOME"
+
 # Install other EverParse deps
 "$build_home"/install-other-deps.sh
