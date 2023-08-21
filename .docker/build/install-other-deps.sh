2060d9b1c2098875182eb91eef3cf0fad296230f
Tahina Ramananandro <taramana@microsoft.com>
2023-08-21 15:22:55 -0700

    install-other-deps should install everything except F*

--- .docker/build/install-other-deps.sh
+++ .docker/build/install-other-deps.sh
@@ -6,7 +6,9 @@ set -x
 build_home="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Identify the Karamel branch
+if [[ -z "$KRML_BRANCH" ]] ; then
 KRML_BRANCH=$(jq -c -r '.RepoVersions.karamel_version' "$build_home"/config.json | sed 's!^origin/!!')
+fi
 
 # Install Karamel and its dependencies
 [[ -n "$KRML_HOME" ]]
@@ -15,5 +17,15 @@ pushd $KRML_HOME
 .docker/build/install-other-deps.sh
 popd
 OTHERFLAGS='--admit_smt_queries true' make -j 24 -C "$KRML_HOME"
+
+# Identify the Steel branch
+if [[ -z "$STEEL_BRANCH" ]] ; then
+STEEL_BRANCH=$(jq -c -r '.RepoVersions.steel_version' "$build_home"/config.json | sed 's!^origin/!!')
+fi
+
+# Install Steel and its dependencies
+[[ -n "$STEEL_HOME" ]]
+git clone --branch $STEEL_BRANCH https://github.com/FStarLang/steel "$STEEL_HOME"
+OTHERFLAGS='--admit_smt_queries true' make -j 24 -C "$STEEL_HOME"
 
 opam install hex re ctypes sha sexplib
